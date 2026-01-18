2a6594291f89bd5ec4fe2e6e7e8a90d2733ebfcb
jackson <korobkoevgeniikor@yandex.ru>
2026-01-18 20:39:55 +0300

    added tools for adding users

---
+++ regPH.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/bash
+dir="PH_keys"
+if [ ! -d "$dir" ]; then
+  mkdir "$dir"
+fi
+
+cd "$dir"
+pwd
+
+if [ -z "$1" ]; then
+  echo "Ошибка: Не указано имя."
+  exit 1  # Выход из скрипта с кодом ошибки 1
+fi
+
+userName=$1
+echo "$userName"
+
+wg genkey | tee /etc/wireguard/"$dir"/"$userName"_privatekey | wg pubkey | tee /etc/wireguard/"$dir"/"$userName"_publickey
+
+userPublicKey=$(cat "$userName"_publickey)
+userPrivateKey=$(cat "$userName"_privatekey)
+serverPublicKey=$(cat ../server_publickey)
+echo "$userPublicKey"
+
+cd ../
+pwd
+
+currentIP=$(cat current_ip)
+echo "$currentIP"
+
+currentMask=$(cat ip_mask)
+
+textToServerConfig="
+[Peer] #$userName ph
+PublicKey = $userPublicKey
+AllowedIPs = $currentIP"
+
+echo "$textToServerConfig" >> wg0.conf
+
+
+newIP=$(./newIp.sh "$currentIP")
+> current_ip
+echo "$newIP" >> current_ip
+
+confDir="PH_confs"
+if [ ! -d "$confDir" ]; then
+  mkdir "$confDir"
+fi
+
+cd  "$confDir"
+
+touch "$userName"_PH.conf
+
+textConf="[Interface]
+PrivateKey = $userPrivateKey
+Address = $currentIP$currentMask
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = $serverPublicKey
+Endpoint = 31.130.153.64:51820
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 20"
+
+> "$userName"_PH.conf
+echo "$textConf" >> "$userName"_PH.conf
+echo "$tectConf"
+
+systemctl restart wg-quick@wg0.service
+
+qrencode -t ansiutf8 -r "$userName"_PH.conf
